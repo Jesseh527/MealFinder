7567884c85db3e34c1c7e54570aa5fdfeda9af0b
Jesseh527 <jesseh52700@gmail.com>
2023-08-28 14:35:02 -0700

    added logos

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image style = {{width:100,height:100}} 
+      source={require('./assets/MealFinderLogo2.png')}/>
       <Text>Open up App.js to start working on your app!</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D3D3D3',
     alignItems: 'center',
     justifyContent: 'center',
   },
