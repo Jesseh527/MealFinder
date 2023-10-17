cc6a8357cb57559d513ae3a06f358178c97281c9
Jesseh527 <Jesseh52700@gmail.com>
2023-10-17 13:45:41 -0700

    got login to update

--- Src/Pages/loginScreen.js
+++ Src/Pages/loginScreen.js
@@ -2,34 +2,53 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import ProfilePicture from '../componets/profilePicture';
-
+import { getDatabase, ref, set } from "firebase/database";
+import {db} from "../componets/config"
 
 const LoginScreen = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const [email, setEmail] = useState(''); // use later
   const handleLogin = () => {
     // Here, you can add logic to authenticate the user
-    if (username === 'your_username' && password === 'your_password') {
-      alert('Login successful!');
-    } else {
-      alert('Login failed. Please check your credentials.');
-    }
+    // if (username === 'your_username' && password === 'your_password') {
+    //   alert('Login successful!');
+    // } else {
+    //   alert('Login failed. Please check your credentials.');
+    // }
+    set(ref(db, 'users/' + username), { // change SET to UPDATE to update current data
+      username: email,
+      email: username,
+      password:password
+    }).then(()=>{
+      //data saved
+      alert('data updated')
+    })
+    .catch((error)=>{
+      //failed
+      alert(error);
+    })
   };
 
   return (
     <View style={styles.container}>
       <Text>Login Screen</Text>
       <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+        style={styles.input}
+      />
+      <TextInput
         placeholder="Username"
-        onChangeText={(text) => setUsername(text)}
         value={username}
+        onChangeText={(username) => setUsername(username)}
         style={styles.input}
       />
       <TextInput
         placeholder="Password"
-        onChangeText={(text) => setPassword(text)}
         value={password}
+        onChangeText={(password) => setPassword(password)}
         secureTextEntry={true}
         style={styles.input}
       />
---
+++ Src/componets/config.jsx
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getDatabase} from "firebase/database"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARBznYzRkVLq-NnJ7J65F0pWEklzY1GP8",
+  authDomain: "mealfinderjessehernandez.firebaseapp.com",
+  projectId: "mealfinderjessehernandez",
+  storageBucket: "mealfinderjessehernandez.appspot.com",
+  messagingSenderId: "626321293981",
+  appId: "1:626321293981:web:1159fa6a195e766b898a74",
+  measurementId: "G-7592LKHTKY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//Initialize  database
+export const db = getDatabase(app);
